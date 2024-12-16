befed8fa75d74c2ebc22d5c0ef9955b8ec05aa85
devCaiodias <caiodiasmartins12@gmail.com>
2024-12-16 14:24:08 -0300

    linpando o codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-// 'use client'
 import styles from './page.module.css'
 import Sidebar from '@/components/Sidebar';
 import Mainn from '@/components/Mainn';
 import Footerr from '@/components/Footerr';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
 
 
 export default function Home() {
-  // const [musicc, setMusicc] = useState([])
-  
-  // useEffect(() => {
-  //   axios.get('https://json-server-vercel-3izoydvww-protagonistaaas-projects.vercel.app/music')
-  //   .then(response => {
-  //     console.log("Dados Recebidos: ", response.data);
-  //     setMusicc(response.data)
-  //   })
-  // })
 
   return (
     <div className={styles.page}>
